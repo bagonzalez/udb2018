6808f15af9e6e2520d13f34dd4293f27db5b9ed9
DanielPalomo115 <dp.palomo115@gmail.com>
2018-08-31 18:13:12 -0600

    Juego terminado temporalmente

--- Assets/Script/GameCon.cs
+++ Assets/Script/GameCon.cs
@@ -2,6 +2,10 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public enum GameState { Idle, playing, Ended };
+
 
 
 public class GameCon : MonoBehaviour {
@@ -10,15 +14,30 @@ public class GameCon : MonoBehaviour {
     public RawImage back;
     public RawImage platf;
     public GameObject uiIdle;
+    public GameObject uiscore;
 
-    public enum GameState {Idle, playing};
+    public Text pointsText;
+    public Text recordText;
+
     public GameState gameState = GameState.Idle;
+
+    public GameObject player;
+    public GameObject enemyGenerator;
+
+    private AudioSource musicPlayer;
+
+    public float scaleTime = 6f;
+    public float scaleInc = .25f;
+
+    private int points = 0;
 
 
 	// Use this for initialization
 	void Start () {
-		
-	}
+        musicPlayer = GetComponent<AudioSource>();
+        recordText.text = "Puntuacion Maxima: " + GetMaxScore().ToString();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
@@ -28,6 +47,13 @@ public class GameCon : MonoBehaviour {
         {
             gameState = GameState.playing;
             uiIdle.SetActive(false);
+
+            uiscore.SetActive(true);
+
+            player.SendMessage("UpdateState", "playerRun");
+            enemyGenerator.SendMessage("StartGenerator");
+            musicPlayer.Play();
+            InvokeRepeating("GameTimeScale", scaleTime, scaleTime);
         } 
 
         //juego en marcha
@@ -36,6 +62,13 @@ public class GameCon : MonoBehaviour {
             parallax();
 
         }
+        else if (gameState == GameState.Ended)
+        {
+            if(Input.GetKeyDown("up") || Input.GetMouseButtonDown(0)){
+                RestartGame();
+            }
+
+            }
     }
 
     void parallax()
@@ -43,5 +76,43 @@ public class GameCon : MonoBehaviour {
         float finalSpeed = parallaxSpeed * Time.deltaTime;
         back.uvRect = new Rect(back.uvRect.x + finalSpeed, 0f, 1f, 1f);
         platf.uvRect = new Rect(platf.uvRect.x + finalSpeed * 4, 0f, 1f, 1f);
+    }
+
+    public void RestartGame()
+    {
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    void GameTimeScale()
+    {
+        Time.timeScale += scaleInc;
+    }
+
+    public void ResetTimeScale()
+    {
+        CancelInvoke("GameTimeScale");
+        Time.timeScale = 1f;
+            
+    }
+
+    public void IncreasePoints()
+    {
+        points++;
+        pointsText.text = points.ToString();
+        if(points >= GetMaxScore())
+        {
+            recordText.text = "Puntuacion Maxima: " + points.ToString();
+            SaveScore(points);
+        }
+    }
+
+    public int GetMaxScore()
+    {
+        return PlayerPrefs.GetInt("Max Points", 0);
+
+    }
+    public void SaveScore(int currentPoints)
+    {
+        PlayerPrefs.SetInt("Max Points", currentPoints);
     }
 }
---
+++ Assets/Script/PlayerCon.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCon : MonoBehaviour {
+
+    private Animator animator;
+    public GameObject game;
+    public GameObject enemyGen;
+    public AudioClip Dieclip;
+    private AudioSource audioPlayer;
+	// Use this for initialization
+	void Start () {
+        animator = GetComponent<Animator>();
+        audioPlayer = GetComponent<AudioSource>();
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        bool gamePlaying = game.GetComponent<GameCon>().gameState == GameState.playing;
+        if (gamePlaying && (Input.GetKeyDown("up") || Input.GetMouseButtonDown(0))){
+            UpdateState("playerJump");
+        }
+		
+	}
+
+    public void UpdateState(string state = null )
+    {
+        if(state != null)
+        {
+            animator.Play(state);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Enemy")
+        {
+            UpdateState("playerDie");
+            game.GetComponent<GameCon>().gameState = GameState.Ended;
+            enemyGen.SendMessage("CancelGenerator", true);
+
+            game.SendMessage("ResetTimeScale");
+
+            game.GetComponent<AudioSource>().Stop();
+            audioPlayer.clip = Dieclip;
+            audioPlayer.Play();
+        }
+        else if (other.gameObject.tag == "point")
+        {
+            game.SendMessage("IncreasePoints");
+        }
+    }
+    void GameReady()
+    {
+        game.GetComponent<GameCon>().gameState = GameState.Ended;
+
+    }
+
+}
