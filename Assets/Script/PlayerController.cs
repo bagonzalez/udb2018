2f28e14fa37e3d7841016a75290a0f39cd312e5c
kevin <sanchez1996kasc@gmail.com>
2018-09-24 21:00:56 -0600

    escena 1, juego en proceso

---
+++ Assets/Script/PlayerController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float spedd;
+    private Rigidbody rb;
+    
+
+	// Use this for initialization
+	void Start () {
+        rb = GetComponent<Rigidbody>();
+	}
+
+     void FixedUpdate()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        Vector3 move = new Vector3(horizontal, 0.0f, vertical);
+        rb.AddForce(move * spedd);
+    }
+
+    // Update is called once per frame
+    void Update () {
+       
+
+
+    }
+}
