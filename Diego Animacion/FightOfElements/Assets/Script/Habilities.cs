ac784a87fd1cc5f4a7411d76336ff9954e487e76
Diegodk <diegodksanchez@gmail.com>
2018-10-15 22:26:32 -0600

    animation and Scenes

---
+++ Diego Animacion/FightOfElements/Assets/Script/Habilities.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Habilities : MonoBehaviour {
+
+	public GameObject Me;
+	public GameObject BaseRocks;
+	public GameObject WallRocks;
+	public GameObject PositionWallRocks;
+	public bool CDBaseRock;
+	public bool CDWallRock;
+	public float TimeCDBaseRock;
+	public float TimeCDWallRock;
+	private Vector3 _positionBaseRock;
+	private Vector3 _positionWallRock;
+	// Use this for initialization
+	void Start () {
+		_positionBaseRock = new Vector3();
+		CDBaseRock = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_positionBaseRock = Me.transform.position;
+		_positionBaseRock.y -= 10;
+
+		_positionWallRock = PositionWallRocks.transform.position;
+		_positionWallRock.y -= 0.5f;
+
+		TimerComparation();
+
+		if (Input.GetKey(KeyCode.LeftControl))
+		{
+			if (CDBaseRock)
+			{
+				Instantiate(BaseRocks, _positionBaseRock, Quaternion.identity);
+				//TimeCDBaseRock = Time.time + 10;
+				TimeCDBaseRock = Time.time + 1;
+				CDBaseRock = false;
+			}
+		}
+
+		if (Input.GetKey(KeyCode.E))
+		{
+			if (CDWallRock)
+			{
+				Instantiate(WallRocks, _positionWallRock, PositionWallRocks.transform.rotation);
+				//TimeCDBaseRock = Time.time + 10;
+				TimeCDWallRock = Time.time + 1;
+				CDWallRock = false;
+			}
+		}
+	}
+
+	private void TimerComparation()
+	{
+		float timeNow = Time.time;
+		Debug.Log("Time" + timeNow);
+		if (timeNow >= TimeCDBaseRock)
+		{
+			CDBaseRock = true;
+		}
+
+		if (timeNow >= TimeCDWallRock)
+		{
+			CDWallRock = true;
+		}
+		
+	}
+}
---
+++ Diego Animacion/FightOfElements/Assets/SelectEscena.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SelectEscena : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	public void LoadOmashu()
+	{
+		SceneManager.LoadScene("Omashu", LoadSceneMode.Single);
+	}
+}
